6440481fa95f196eabb8f248d9a38c0baf328e31
Scott Tesoriere <scott@keepflippin.com>
2009-04-15 10:19:34 -0500

    no longer using system('curl')

--- index.php
+++ index.php
@@ -11,12 +11,41 @@ class GithubActiveCollab {
 
 		if (!$res = json_decode(stripslashes($payload), true))
 			exit;
-		print_r($res);
+		//print_r($res);
 		foreach ($res['commits'] as &$commit) {
 			$this->process_commit($commit);
 		}
 
 	}
+	
+	function _post($url, $header, $data) {
+		$options = array(
+			CURLOPT_RETURNTRANSFER 	=> true,
+			CURLOPT_HEADER			=> false,
+			CURLOPT_CONNECTTIMEOUT 	=> 120,
+	        CURLOPT_TIMEOUT        	=> 120,
+			CURLOPT_POST			=> 1,
+			CURLOPT_POSTFIELDS		=> $data,
+			CURLOPT_SSL_VERIFYHOST 	=> 0,
+	        CURLOPT_SSL_VERIFYPEER	=> false,
+			CURLOPT_HTTPHEADER 		=> array($header),
+	        CURLOPT_VERBOSE       	=> 1		
+			);
+		$curl = curl_init($url);
+		curl_setopt_array($curl, $options);
+		$content = curl_exec($curl);
+		if (!$errno = curl_errno($curl)) {
+		}
+		$errmsg = curl_error($curl);
+		echo $errno.' '.$errmsg."\n";
+		echo $data; 
+		$response = curl_getinfo($curl);
+		curl_close($curl);
+		echo $content;
+
+
+	}
+	
 	
 	function process_commit($commit) {
 
@@ -24,8 +53,9 @@ class GithubActiveCollab {
 		$files = implode(", ",$commit['removed']) . implode(", ",$commit['added']) . implode(", ",$commit['modified']);
 
 	    $url  = $this->config['submit_url'].'/'.$this->config['type'].'s/add?token='.$this->config['token'];
-		$post = '"submitted=submitted&'.$this->config['type'].'[name]='.$commit['id'].' | '.$message.'&'.$this->config['type'].'[body]=Files: '.$files.'"';
+		$post = 'submitted=submitted&'.$this->config['type'].'[name]='.urlencode($message).'+|+'.$commit['id'].' by '.urlencode($commit['author']['name']).'&'.$this->config['type'].'[body]='.urlencode('Files: '.$files);
 		$curl = $this->config['curl'].' --insecure --silent -d '.$post.' -X POST -H "Accept:application/json" '.$url;
+		$this->_post($url, 'Accept: application/json', $post);
 		// echo $curl;
 	    //echo system($curl);
 	}
@@ -33,11 +63,9 @@ class GithubActiveCollab {
 		
 }
 
-function ob_callback($buffer) {
-	global $ob_file;
-	fwrite($ob_file, $buffer);
-}
-
+if (count($_POST) <= 0)
+	die ('Hi!');
+	
 $ob_file = fopen('development.log', 'a');
 ob_start();
 
