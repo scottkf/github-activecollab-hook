2488fea0996deaaae20e25671a5913bdce1e614a
Scott Tesoriere <scott@Scott-Tesorieres-Macbook-Pro.local>
2009-04-15 21:12:00 -0400

    don't try to use functions as variables. I hate php.

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ include ('spyc/spyc.php');
 class GithubActiveCollab {
 	protected $config;
 
+
 	function __construct($payload) {
+
 		$this->config = Spyc::YAMLLoad('config.yml');
-		echo 'here';
+
 
 		if (!$res = json_decode(stripslashes($payload), true))
 			die('your json was ;(');
@@ -46,34 +48,31 @@ class GithubActiveCollab {
 	}
 
 	function process_commit($commit) {
-
 		// need to clean this up to it constructs automatically off of an array
 		$type = $this->config['type'];
 		$token = $this->config['token'];
 		// this is the user who owns the token (first 2 digits of the token), hackish way to do it now but it works, read below to fix
-		$user = $substr($token, 0, 2);
+		$user = substr($token, 0, 2);
 
+		// process this thing looking for a lighthouse like thing
 		$message = $commit['message'];
-		
-		// process the thing looking for a lighthouse like thing
 	
-		echo 'here';
 		
 		$files = "<b>Removed</b>:\n\t".implode(", ",$commit['removed']) ."\n<b>Added</b>\n\t". implode(", ",$commit['added']) ."\n<b>Modified</b>\n\t". implode(", ",$commit['modified']);
 
 	    $url  = $this->config['submit_url'].'/'.$type.'s/add?token='.$token;
 
 
-		echo 'before';
 		$post = 'submitted=submitted&'.
 				$type.'[name]='.urlencode($message).'+|+'.$commit['id'].' by '.urlencode($commit['author']['name']).'&'.
 				$type.( $this->config['type'] != 'discussion' ? '[body]' : '[message]').'='.urlencode($commit['url'])."\n".urlencode("<b>Files:</b>\n".$files).
 				($this->config['category'] > 0 ? '&'.$type.'[parent_id]='.$this->config['category'] : '').
-				// assign whoever owns the token to the ticket, change this to use the yml config, 
-				//    or eventually set it up to pull the user if it exists and use that user
-				//    ie, /people, then search through the list
 				($type == 'ticket' ? '&ticket[assignees][0][]='.$user.'&ticket[assignees][1]='.$user : '');
-		echo 'after';
+			// assign whoever owns the token to the ticket, change this to use the yml config, 
+			//    or eventually set it up to pull the user if it exists and use that user
+			//    ie, /people, then search through the list
+
+
 		// need to implement a real pluralize function above to resolve any idiocy which might occur
 		$response = json_decode(stripslashes($this->_post($url, $post)), true);
 		print_r($response);
@@ -90,9 +89,9 @@ class GithubActiveCollab {
 if (count($_POST) <= 0)
 	die ('Hi!');
 
-$ob_file = fopen('development.log', 'a');
+$ob_file = fopen('development.log', 'a+');
 ob_start();
-echo 'here';
+
 $gh = new GithubActiveCollab($_POST['payload']);
 
 fwrite($ob_file, ob_get_contents());
